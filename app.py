11844ff30cc82fc493730c6d9a6ba55f663173a2
Bilal Kaludi <63890666+Kaludii@users.noreply.github.com>
2023-02-13 15:55:58 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 st.title("OpenAI Chatbot")
-st.write("Interact with OpenAI's models in real-time using your OpenAI API. Choose from a selection of their best models, set the temperature and max tokens, and start a conversation. Delete the conversation at any time and start fresh.")
+st.write("Interact with OpenAI's GPT-3 models in real-time using your OpenAI API. Choose from a selection of their best models, set the temperature and max tokens, and start a conversation. Delete the conversation at any time and start fresh.")
 
 if "history" not in st.session_state:
     st.session_state.history = []
